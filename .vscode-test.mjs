7cc7222459d7e8e8ce4852a6af62f62b48bf9fc2
John Brown <john.brown@krakenflex.com>
2024-09-22 21:59:43 +0100

    feat: identified vscode engine

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from '@vscode/test-cli';
 export default defineConfig({
   label: 'unitTests',
   files: 'out/test/**/*.test.js',
+  workspaceFolder: 'out/',
   mocha: {
     ui: 'tdd',
     timeout: 20000
-  }
+  },
+  version: '1.72.0'
 });
